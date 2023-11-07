46621a6fb457bb30855db0e91f8b7a7efd682f3a
Rafael Ehlert <ra.faehlert@hotmail.com>
2023-11-07 15:42:29 -0300

    H2 database

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -11,7 +11,7 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "tb_categories")
 public class Category implements Serializable{
-    private static final long serialVersionuid = 1L;
+    private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
