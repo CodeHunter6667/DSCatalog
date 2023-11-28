327c33bc2a62362f2d597c7a2964279c3424b522
Rafael Ehlert <ra.faehlert@hotmail.com>
2023-11-28 15:11:34 -0300

    Find by Id

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -3,6 +3,7 @@ package com.devsuperior.dscatalog.services;
 import java.util.List;
 
 import com.devsuperior.dscatalog.dto.CategoryDTO;
+import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,5 +22,13 @@ public class CategoryService {
     public List<CategoryDTO> findAll(){
         List<Category> list = repository.findAll();
         return list.stream().map(CategoryDTO::new).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public CategoryDTO findById(Long id){
+        Category category = repository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Categoria não encontrada")
+        );
+        return new CategoryDTO(category);
     }
 }
